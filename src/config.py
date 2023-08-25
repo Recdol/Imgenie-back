166eddb7121e3722f8cf02aa4e56ac0913360c0a
2jun0 <soo28819@gmail.com>
2023-08-25 16:43:25 +0900

    IMR-186 refactor: move pl_k to config

--- src/config.py
+++ src/config.py
@@ -30,6 +30,8 @@ class AppConfig(BaseSettings):
     sit_index_version: str = "sit-25_133334"
     mood_index_version: str = "mood-25_144428"
 
+    playlist_k: int = 6
+
     # image
     image_size: int = 224
 
