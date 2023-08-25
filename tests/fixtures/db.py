06cf346f8397a5b8298577f40aaac8cf3e439173
2jun0 <soo28819@gmail.com>
2023-08-26 03:33:11 +0900

    IMR-186 test: add test for feedback

---
+++ tests/fixtures/db.py
@@ -0,0 +1,110 @@
+import pytest
+from unittest.mock import MagicMock
+from datetime import datetime, date
+
+from src.db import (
+    User,
+    Artist,
+    Album,
+    Song,
+    Playlist,
+    Inference,
+    InferenceQuery,
+    InferenceOutput,
+    InferenceFeedback,
+    InferenceRepository,
+    SongRepository,
+)
+
+
+@pytest.fixture
+def user() -> User:
+    return User(id="user")
+
+
+@pytest.fixture
+def artist() -> Artist:
+    return Artist(
+        id="artist",
+        genie_id="g_artist",
+        name="주혜인",
+        created_at=datetime.utcnow(),
+        updated_at=datetime.utcnow(),
+    )
+
+
+@pytest.fixture
+def album() -> Album:
+    return Album(
+        id="album",
+        genie_id="g_album",
+        name="민석05의 앨범",
+        img_url="http://img.png",
+        released_date=date.today(),
+        created_at=datetime.utcnow(),
+        updated_at=datetime.utcnow(),
+    )
+
+
+@pytest.fixture
+def song(artist: Artist, album: Album) -> Song:
+    return Song(
+        id="song",
+        genie_id="g_song",
+        title="찬미송",
+        lyrics="가사는 어디까지 길어질 수 있을까?를 시험해보고 싶지만 그냥 귀찮아서 여기까지 적는다. 유후후~",
+        album=album,
+        artist=artist,
+        like_cnt=10,
+        listener_cnt=20,
+        play_cnt=100,
+        genres=["POP"],
+        spotify_url="http://spotify.com/123.mp3",
+        created_at=datetime.utcnow(),
+        updated_at=datetime.utcnow(),
+    )
+
+
+@pytest.fixture
+def playlist(song: Song) -> Playlist:
+    return Playlist(
+        id="playlist",
+        genie_id="g_playlist",
+        title="동맘의 뽕짝 1선",
+        subtitle="동여니가 젤 좋아하는 노래로 준비했어요~ ^^",
+        song_cnt=1,
+        like_cnt=99,
+        view_cnt=100,
+        tags=["행복"],
+        songs=[song],
+        img_url="http://img/img.jpg",
+        created_at=datetime.utcnow(),
+        updated_at=datetime.utcnow(),
+    )
+
+
+@pytest.fixture
+def inference(user: User, song: Song, playlist: Playlist) -> Inference:
+    return Inference(
+        id="inference",
+        user=user,
+        query=InferenceQuery(image_url="http://img/query.jpg", genres=["POP"]),
+        output=InferenceOutput(playlists=[playlist], songs=[song]),
+        feedback=InferenceFeedback(like_songs=[]),
+        created_at=datetime.utcnow(),
+        updated_at=datetime.utcnow(),
+    )
+
+
+@pytest.fixture
+def mock_inference_repository(inference: Inference) -> InferenceRepository:
+    mock = MagicMock(spec=InferenceRepository)
+    mock.create_inference.return_value = inference
+    return mock
+
+
+@pytest.fixture
+def mock_song_repository(song: Song) -> SongRepository:
+    mock = MagicMock(spec=SongRepository)
+    mock.find_by_genie_id.return_value = song
+    return mock
---
+++ tests/services/test_feedback.py
@@ -0,0 +1,74 @@
+import pytest
+from logging import Logger
+from unittest.mock import MagicMock
+
+from src.services.feedback import FeedbackService
+from src.db import User, Song, Inference, InferenceRepository, SongRepository
+from src.dto.request import UserFeedbackRequest
+
+
+@pytest.fixture
+def feedback_service(
+    mock_logger: Logger,
+    mock_inference_repository: InferenceRepository,
+    mock_song_repository: SongRepository,
+) -> FeedbackService:
+    return FeedbackService(
+        feedback_logger=mock_logger,
+        inference_repository=mock_inference_repository,
+        song_repository=mock_song_repository,
+    )
+
+
+@pytest.fixture
+def like_user_feedback_request(
+    feedback_song: Song, inference: Inference
+) -> UserFeedbackRequest:
+    return UserFeedbackRequest(
+        song_id=feedback_song.id, inference_id=inference.id, is_like=True
+    )
+
+
+@pytest.fixture
+def dislike_user_feedback_request(
+    feedback_song: Song, inference: Inference
+) -> UserFeedbackRequest:
+    return UserFeedbackRequest(
+        song_id=feedback_song.id, inference_id=inference.id, is_like=False
+    )
+
+
+@pytest.fixture
+def feedback_song(inference: Inference) -> Song:
+    # 피드백 받을 대상 노래
+    return inference.output.songs[0]
+
+
+def test_user_feedback__should_add_song_in_like_songs_when_is_like_is_true(
+    feedback_service: FeedbackService,
+    user: User,
+    feedback_song: Song,
+    inference: Inference,
+    like_user_feedback_request: UserFeedbackRequest,
+    mock_inference_repository: MagicMock | InferenceRepository,
+):
+    feedback_service.user_feedback(user, request=like_user_feedback_request)
+
+    mock_inference_repository.add_feedback_like_song_by_id.assert_called_once_with(
+        inference.id, feedback_song
+    )
+
+
+def test_user_feedback__should_add_song_in_like_songs_when_is_like_is_false(
+    feedback_service: FeedbackService,
+    user: User,
+    feedback_song: Song,
+    inference: Inference,
+    dislike_user_feedback_request: UserFeedbackRequest,
+    mock_inference_repository: MagicMock | InferenceRepository,
+):
+    feedback_service.user_feedback(user, request=dislike_user_feedback_request)
+
+    mock_inference_repository.delete_feedback_like_song.assert_called_once_with(
+        inference.id, feedback_song
+    )
