ece902efc4be667ee8a421a5ca77f596883d9d41
2jun0 <soo28819@gmail.com>
2023-09-05 00:23:23 +0900

    refactor: remove feedback logger

--- src/services/feedback.py
+++ src/services/feedback.py
@@ -1,5 +1,3 @@
-from logging import Logger
-
 from ..dto.request import UserFeedbackRequest
 from ..db import User, InferenceRepository, SongRepository
 
@@ -7,11 +5,9 @@ from ..db import User, InferenceRepository, SongRepository
 class FeedbackService:
     def __init__(
         self,
-        feedback_logger: Logger,
         inference_repository: InferenceRepository,
         song_repository: SongRepository,
     ) -> None:
-        self.feedback_logger = feedback_logger
         self.inference_repository = inference_repository
         self.song_repository = song_repository
 
@@ -26,11 +22,3 @@ class FeedbackService:
             self.inference_repository.delete_feedback_like_song(
                 request.inference_id, song
             )
-
-        self.feedback_logger.info(
-            {
-                "User Id": user.id,
-                "song Id": request.song_id,
-                "is like": request.is_like,
-            }
-        )
