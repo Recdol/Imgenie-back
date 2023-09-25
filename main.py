fc2a52029e6348520f795b6aaa0dc49dd6a39766
2jun0 <soo28819@gmail.com>
2023-09-25 12:48:09 +0900

    refactor: remove CORS

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+
 from src import api, db
 from src.exception_handler import add_exception_handler
-from src.config import AppConfig
+from src.config import get_app_config
 
-config = AppConfig()
+config = get_app_config()
 
 db.connect(
     db=config.db_name,
@@ -16,7 +16,6 @@ db.connect(
 
 app = FastAPI()
 app.include_router(api.router)
-app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 add_exception_handler(app)
 
 if __name__ == "__main__":
